7be1e05674750c43fa0c2bdb9dea06aa3c42f2b9
slyeet03 <shobhit7419@gmail.com>
2026-02-21 00:28:10 +0530

    can see a window now

--- src/main.c
+++ src/main.c
@@ -6,14 +6,36 @@
 
 #define SDL_FLAGS SDL_INIT_VIDEO
 
-struct Game {
+#define WINDOW_TITLE "Game"
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+typedef struct {
   SDL_Window *window;
   SDL_Renderer *renderer;
-};
+} Game;
 
-bool game_init_sdl() {
+// declaring all the functions cause compiler is stupid
+bool init_sdl(Game *g);
+void game_free(Game *g);
+void run(Game *g);
+
+// to initialize sdl
+bool init_sdl(Game *g) {
   if (!SDL_Init(SDL_FLAGS)) {
     fprintf(stderr, "Error intializing SDL: %s\n", SDL_GetError());
+    return false;
+  }
+
+  g->window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+  if (!g->window) {
+    fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+    return false;
+  }
+
+  g->renderer = SDL_CreateRenderer(g->window, NULL);
+  if (!g->renderer) {
+    fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
     return false;
   }
 
@@ -21,16 +43,51 @@ bool game_init_sdl() {
 }
 
 // to free all the memory
-void game_free() { SDL_Quit(); }
+void game_free(Game *g) {
+  if (g->renderer) {
+    SDL_DestroyRenderer(g->renderer);
+    g->renderer = NULL;
+  }
+  if (g->window) {
+    SDL_DestroyWindow(g->window);
+    g->window = NULL;
+  }
+
+  SDL_Quit();
+}
+
+void run(Game *g) {
+  SDL_Event event;
+  bool running = true;
+
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = false;
+      }
+      if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) {
+        running = false;
+      }
+    }
+
+    SDL_SetRenderDrawColor(g->renderer, 0, 0, 0, 255);
+    SDL_RenderClear(g->renderer);
+    SDL_RenderPresent(g->renderer);
+  }
+}
 
 int main() {
   bool exit_status = EXIT_FAILURE;
 
-  if (game_init_sdl()) {
+  Game game = {0};
+
+  if (init_sdl(&game)) {
+    run(&game);
+
     exit_status = EXIT_SUCCESS;
   }
 
-  game_free();
+  game_free(&game);
 
   return exit_status;
 }
